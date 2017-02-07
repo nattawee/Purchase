6c833660ee7e87d47e0895a02330d59e8e157d6a
Jigkoh 0220 <Jigkoh 0220>
2017-02-07 08:25:46 +0700

    approver

--- modules/renovates/server/policies/renovates.server.policy.js
+++ modules/renovates/server/policies/renovates.server.policy.js
@@ -22,7 +22,7 @@ exports.invokeRolesPolicies = function () {
       permissions: '*'
     }]
   }, {
-    roles: ['user'],
+    roles: ['user', 'approver'],
     allows: [{
       resources: '/api/renovates',
       permissions: ['get', 'post']
