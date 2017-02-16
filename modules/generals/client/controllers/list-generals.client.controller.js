08746123e5051b5ca4a81fdc161e738971e77ac1
Bu4y <amnes1a@windowslive.com>
2017-02-16 11:35:20 +0700

    export excel

--- modules/generals/client/controllers/list-generals.client.controller.js
+++ modules/generals/client/controllers/list-generals.client.controller.js
@@ -44,12 +44,12 @@
                   onlinedate: general.announcement ? new Date(general.announcement.onlinedate).toLocaleDateString() : '',
                   reference: general.announcement ? general.announcement.reference : '',
                   pamount: general.purchase.amount,
-                  papprvdate: general.purchase ? new Date(general.purchase.apprvdate).toLocaleDateString(): '',
+                  papprvdate: general.purchase.apprvdate ? new Date(general.purchase.apprvdate).toLocaleDateString() : '',
                   papprover: general.purchase.approver,
                   supplier: general.supplier,
                   refno: general.refno,
                   naccdocdate: general.nacc ? new Date(general.nacc.naccdocdate).toLocaleDateString() : '',
-                  naccdocno: general.nacc ? general.nacc.naccdocno :''
+                  naccdocno: general.nacc ? general.nacc.naccdocno : ''
                 };
                 // if (general.department.name === vm.authentication.department){
                 vm.dataList.push(data);
